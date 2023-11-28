9da57a61b77e8252d8c7263668ce816833eb9805
Aleena Rose <aleenarose3112@gmail.com>
2023-11-28 08:50:11 +0300

    code for converting celcius to farenheit and vice versa

---
+++ checktemp.py
@@ -0,0 +1,5 @@
+import mytemp as t
+c=int(input("enter your temp in c and we will provide the temp:"))
+print(c,"celsius is equivalent to ",t.celtofeh(c))
+f=int(input("enter your temp in fh and we will provide the c temp"))
+print(f,"fh is equivalent to",t.fehtocel(f))
---
+++ mytemp.py
@@ -0,0 +1,6 @@
+def celtofeh(ctemp):
+    ftemp=9/5*ctemp+32
+    return ftemp
+def fehtocel(ftemp):
+    ctemp=(ftemp-32)*5/9
+    return ctemp
